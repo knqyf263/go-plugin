c99ca8608e702304a2ab243090c34b8dc23c514a
knqyf263 <knqyf263@gmail.com>
2022-08-25 02:41:46 +0300

    Add field tests

--- go.mod
+++ go.mod
@@ -4,6 +4,13 @@ go 1.18
 
 require (
 	github.com/planetscale/vtprotobuf v0.3.0
+	github.com/stretchr/testify v1.7.1
 	github.com/tetratelabs/wazero v0.0.0-20220819101114-6c2712fd00fc
 	google.golang.org/protobuf v1.28.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
---
+++ test/fields/plugin/plugin.go
@@ -0,0 +1,53 @@
+//go:build tinygo.wasm
+
+package main
+
+import (
+	"context"
+
+	"github.com/knqyf263/go-plugin/test/fields/proto"
+)
+
+// main is required for TinyGo to compile to Wasm.
+func main() {
+	proto.RegisterFieldTest(TestPlugin{})
+
+}
+
+type TestPlugin struct{}
+
+func (p TestPlugin) Test(_ context.Context, request proto.Request) (proto.Response, error) {
+	return proto.Response{
+		A: request.GetA() * 2,
+		B: request.GetB() * 2,
+		C: request.GetC() * 2,
+		D: request.GetD() * 2,
+		E: request.GetE() * 2,
+		F: request.GetF() * 2,
+		G: request.GetG() * 2,
+		H: request.GetH() * 2,
+		I: request.GetI() * 2,
+		J: request.GetJ() * 2,
+		K: request.GetK() * 2,
+		L: request.GetL() * 2,
+		M: !request.GetM(),
+		N: request.GetN() + "bar",
+		O: append(request.GetO(), []byte("fuga")...),
+		P: request.GetP()[1:],
+		Q: func() map[string]*proto.IntValue {
+			q := request.GetQ()
+			q["key"].A++
+			return q
+		}(),
+		R: func() *proto.Response_Nested {
+			r := request.GetR()
+			if r.A == "samurai" {
+				return &proto.Response_Nested{
+					A: "ninja",
+				}
+			}
+			return nil
+		}(),
+		S: request.GetS() + 1,
+	}, nil
+}
