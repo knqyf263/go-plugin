7f974c5fc5851fdc658ececb96b3c4f682d1e236
knqyf263 <knqyf263@gmail.com>
2022-08-27 15:24:31 +0300

    Add a test for importing proto files

---
+++ test/import/plugin/plugin.go
@@ -0,0 +1,24 @@
+//go:build tinygo.wasm
+
+package main
+
+import (
+	"context"
+
+	"github.com/knqyf263/go-plugin/test/import/proto/bar"
+	"github.com/knqyf263/go-plugin/test/import/proto/foo"
+)
+
+// main is required for TinyGo to compile to Wasm.
+func main() {
+	foo.RegisterFoo(TestPlugin{})
+
+}
+
+type TestPlugin struct{}
+
+func (p TestPlugin) Hello(_ context.Context, request foo.Request) (bar.Reply, error) {
+	return bar.Reply{
+		A: request.GetA() + ", bar",
+	}, nil
+}
