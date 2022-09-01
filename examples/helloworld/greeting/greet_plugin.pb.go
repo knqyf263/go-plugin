6e904e6dcc616daea5c7b706e6927c1bdc8c664e
Takeshi Yoneda <takeshi@tetrate.io>
2022-09-01 11:24:40 +0900

    dep: updates wazero to the latest
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- examples/helloworld/greeting/greet_plugin.pb.go
+++ examples/helloworld/greeting/greet_plugin.pb.go
@@ -4,7 +4,7 @@
 // versions:
 // 	protoc-gen-go-plugin v0.1.0
 // 	protoc               v3.21.5
-// source: greeting/greet.proto
+// source: examples/helloworld/greeting/greet.proto
 
 package greeting
 
