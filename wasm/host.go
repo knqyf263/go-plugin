e0c3a9313346bbed2f90aed06fb6b8cec4819935
Crypt Keeper <64215+codefromthecrypt@users.noreply.github.com>
2023-01-18 07:47:47 -0600

    deps: updates wazero to 1.0.0-pre.7 (#18)
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- wasm/host.go
+++ wasm/host.go
@@ -12,8 +12,8 @@ import (
 	"github.com/tetratelabs/wazero/api"
 )
 
-func ReadMemory(ctx context.Context, m api.Module, offset, size uint32) ([]byte, error) {
-	buf, ok := m.Memory().Read(ctx, offset, size)
+func ReadMemory(mem api.Memory, offset, size uint32) ([]byte, error) {
+	buf, ok := mem.Read(offset, size)
 	if !ok {
 		return nil, fmt.Errorf("Memory.Read(%d, %d) out of range", offset, size)
 	}
@@ -38,9 +38,9 @@ func WriteMemory(ctx context.Context, m api.Module, data []byte) (uint64, error)
 	dataPtr := results[0]
 
 	// The pointer is a linear memory offset, which is where we write the name.
-	if !m.Memory().Write(ctx, uint32(dataPtr), data) {
+	if !m.Memory().Write(uint32(dataPtr), data) {
 		return 0, fmt.Errorf("Memory.Write(%d, %d) out of range of memory size %d",
-			dataPtr, len(data), m.Memory().Size(ctx))
+			dataPtr, len(data), m.Memory().Size())
 	}
 
 	return dataPtr, nil
