654fc3056fd213d1594c737b9268e1bfef39015b
Dmitry Volodin <dmvolod@gmail.com>
2023-03-14 18:19:28 +0300

    Add empty request tests

--- tests/host-functions/host_functions_test.go
+++ tests/host-functions/host_functions_test.go
@@ -41,6 +41,21 @@ func TestHostFunctions(t *testing.T) {
 	assert.Equal(t, want, reply.GetMessage())
 }
 
+func TestEmptyRequest(t *testing.T) {
+	ctx := context.Background()
+	p, err := proto.NewGreeterPlugin(ctx)
+	require.NoError(t, err)
+
+	plugin, err := p.Load(ctx, "plugin-empty/plugin.wasm", myEmptyHostFunctions{})
+	require.NoError(t, err)
+	defer plugin.Close(ctx)
+
+	reply, err := plugin.Greet(ctx, nil)
+	require.NoError(t, err)
+	want := "Hello, empty request '' and empty '' host function request"
+	assert.Equal(t, want, reply.GetMessage())
+}
+
 // myHostFunctions implements proto.HostFunctions
 type myHostFunctions struct{}
 
@@ -54,4 +69,13 @@ func (myHostFunctions) ParseJson(_ context.Context, request *proto.ParseJsonRequ
 	}
 
 	return &proto.ParseJsonResponse{Response: &person}, nil
+}
+
+type myEmptyHostFunctions struct{}
+
+var _ proto.HostFunctions = (*myEmptyHostFunctions)(nil)
+
+// ParseJson is embedded into the plugin and can be called by the plugin.
+func (myEmptyHostFunctions) ParseJson(_ context.Context, _ *proto.ParseJsonRequest) (*proto.ParseJsonResponse, error) {
+	return &proto.ParseJsonResponse{Response: &proto.Person{}}, nil
 }
---
+++ tests/host-functions/plugin-empty/plugin.go
@@ -0,0 +1,33 @@
+//go:build tinygo.wasm
+
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/knqyf263/go-plugin/tests/host-functions/proto"
+)
+
+// main is required for TinyGo to compile to Wasm.
+func main() {
+	proto.RegisterGreeter(TestPlugin{})
+}
+
+type TestPlugin struct{}
+
+var _ proto.Greeter = (*TestPlugin)(nil)
+
+func (p TestPlugin) Greet(ctx context.Context, request *proto.GreetRequest) (*proto.GreetReply, error) {
+	hostFunctions := proto.NewHostFunctions()
+
+	// Call the host function with nil request
+	resp, err := hostFunctions.ParseJson(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &proto.GreetReply{
+		Message: fmt.Sprintf("Hello, empty request '%s' and empty '%s' host function request", request.GetName(), resp.GetResponse().GetName()),
+	}, nil
+}
